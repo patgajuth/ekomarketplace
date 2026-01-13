31393f376e6661970a982b90e460d6f9c6c756d8
Patryk Gajewski <patgaj15@gmail.com>
2026-01-14 00:25:55 +0100

    style(tiles): center text and reduce size

--- components/home/Tile.tsx
+++ components/home/Tile.tsx
@@ -12,13 +12,13 @@ function Tile({ imageURL, title, className, showTitle = true, containerClassName
   return (
     <div
       className={clsx(
-        "eco-card flex flex-col gap-y-6 items-center justify-center h-[190px] w-[220px] rounded-2xl cursor-pointer",
+        "eco-card flex flex-col gap-y-6 items-center justify-center text-center h-[190px] w-[220px] rounded-2xl cursor-pointer",
         containerClassName
       )}
     >
       <img src={imageURL} alt={`Zdjęcie ${title}`} className={clsx("rounded-lg object-contain", className)} />
       {showTitle ? (
-        <span className="heading6 font-medium text-[var(--textColor-primary)]">{title}</span>
+        <span className="textS font-medium text-[var(--textColor-primary)]">{title}</span>
       ) : (
         <span className="sr-only">{title}</span>
       )}
